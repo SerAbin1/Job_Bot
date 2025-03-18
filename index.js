14a8e23487f758713ce585b37129042af8fa1a78
SerAbinU1 <abinyt1234@gmail.com>
2025-03-18 16:26:34 +0000

    Add /postjob functionality
    Getting Job title and Max applicants correctly from user

--- index.js
+++ index.js
@@ -6,7 +6,27 @@ const TOKEN = process.env.TELEGRAM_TOKEN;
 const bot = new telegramBot(TOKEN, {polling: true});
 
 //reply to any message
-bot.on('message', (msg) => {
+/*bot.on('message', (msg) => {
     const chatId = msg.chat.id;
     bot.sendMessage(chatId, 'I do not recognise that command. Please type /help for a list of commands');
+});*/
+
+//post job
+bot.onText(/\/postjob/, function postJob(msg) {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Enter the job title');
+    
+    //wait for the job title
+    bot.once('message', (msg) => {
+        const jobTitle = msg.text;
+        console.log(jobTitle);
+
+        bot.sendMessage(chatId, 'Enter the maximum number of apllicants');
+
+        //wait for the max number of applicants
+        bot.once('message', (msg) => {
+            const maxApplicants = msg.text;
+            console.log(maxApplicants);
+        });
+    });
 });
